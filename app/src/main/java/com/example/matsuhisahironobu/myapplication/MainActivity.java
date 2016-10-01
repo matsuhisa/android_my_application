9073a7278be8cd09a1678c21e490376a06107ff5
matsuhisa <matsuhisa+github@gmail.com>
2016-10-02 01:25:09 +0900

    Add DisplayMessageActivity

--- app/src/main/java/com/example/matsuhisahironobu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/matsuhisahironobu/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
